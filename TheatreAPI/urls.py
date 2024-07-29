11804b9d01c6da5cb3d3eb242c47b50f6ea70416
Olga <olgahrytsiuk777@gmail.com>
2024-07-29 21:57:28 +0200

    email instead of Username

--- TheatreAPI/urls.py
+++ TheatreAPI/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/user/", include("users.urls", namespace="users")),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
