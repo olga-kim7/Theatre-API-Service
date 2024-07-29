3c315381fde1dc9b335e5d8f500c96b7752a2269
Olga <olgahrytsiuk777@gmail.com>
2024-07-29 18:04:23 +0200

    added a function clean and save for Ticket

--- theatre/models.py
+++ theatre/models.py
@@ -1,27 +1,42 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
-
-
-class Play(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
 
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    plays = models.ManyToManyField(Play, related_name="actors")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
-    plays = models.ManyToManyField(Play, related_name="genres")
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
 
 
 class TheatreHall(models.Model):
     name = models.CharField(max_length=255)
     row = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.row * self.seats_in_row
+
+    def __str__(self):
+        return self.name
 
 
 class Performance(models.Model):
@@ -34,9 +49,53 @@ class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
     performance = models.ForeignKey(Performance, on_delete=models.CASCADE, related_name="tickets")
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    def clean(self):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (self.row, "row", "row"),
+            (self.seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(
+                self.performance.theatre_hall, theatre_hall_attr_name
+            )
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise ValidationError(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
