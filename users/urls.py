69eb299a574b590dd99cc5f9331c07c97699e0a8
Olga <olgahrytsiuk777@gmail.com>
2024-07-29 21:26:44 +0200

    add serializers.py/views.py/urls.py for app users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from users.views import CreateUserView, LoginUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+
+app_name = "user"
