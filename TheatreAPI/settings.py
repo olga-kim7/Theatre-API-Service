4596b601266a876a6322deb28fb81e6367345e12
Olga <olgahrytsiuk777@gmail.com>
2024-07-31 22:38:00 +0200

    add README.md

--- TheatreAPI/settings.py
+++ TheatreAPI/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 INTERNAL_IPS = [
     "127.0.0.1"
