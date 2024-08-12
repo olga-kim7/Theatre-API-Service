bd98363f4cb0924b2aed71af6de7b36350aa0566
Olga <olgahrytsiuk777@gmail.com>
2024-08-12 16:13:58 +0200

    project Theatre API

--- TheatreAPI/settings.py
+++ TheatreAPI/settings.py
@@ -18,16 +18,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
-SECRET_KEY = (
-    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 DEBUG = True
@@ -91,23 +89,17 @@ WSGI_APPLICATION = "TheatreAPI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ["POSTGRES_DB"],
-#         "USER": os.environ["POSTGRES_USER"],
-#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-#         "HOST": os.environ["POSTGRES_HOST"],
-#         "PORT": os.environ["POSTGRES_PORT"],
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- theatre/tests/test_ticket_reservation_api.py
+++ theatre/tests/test_ticket_reservation_api.py
@@ -10,8 +10,10 @@ from theatre.serializers import TicketSerializer
 
 TICKET_LIST_URL = reverse("theatre:ticket-list")
 
+
 def get_ticket_retrieve_url(ticket_id):
     return reverse("theatre:ticket-detail", args=[ticket_id])
+
 
 def sample_reservation(user, **params):
     default = {
@@ -28,10 +30,13 @@ def sample_performance(play, theatre_hall, **params):
     default.update(params)
     return Performance.objects.create(play=play, theatre_hall=theatre_hall, **default)
 
+
 def sample_genre(**params):
     default = {"name": "test genre"}
     default.update(params)
     return Genre.objects.create(**default)
+
+
 def sample_play(**params):
     default = {
         "title": "test title",
@@ -39,6 +44,8 @@ def sample_play(**params):
     }
     default.update(params)
     return Play.objects.create(**default)
+
+
 def sample_theatre_hall(**params):
     default = {
         "name": "test theatre_hall",
@@ -47,6 +54,7 @@ def sample_theatre_hall(**params):
     }
     default.update(params)
     return TheatreHall.objects.create(**default)
+
 
 def sample_ticket(performance, reservation, **params):
     default = {
@@ -58,10 +66,12 @@ def sample_ticket(performance, reservation, **params):
     default.update(params)
     return Ticket.objects.create(**default)
 
+
 class UnAuthorizedTestCase(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.client.force_authenticate(user=None)
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.client.force_authenticate(user=None)
 
     def test_unauthorized_ticket_list(self):
         response = self.client.get(TICKET_LIST_URL)
@@ -71,25 +81,23 @@ class UnAuthorizedTestCase(APITestCase):
         response = self.client.get(get_ticket_retrieve_url(1))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class AuthorizedTestCase(APITestCase):
-    def setUp(self):
-        self.user1 = get_user_model().objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = get_user_model().objects.create_user(
             password="PASSWORD",
             email="mail@test.com"
         )
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user1)
-        self.user2 = get_user_model().objects.create_user(
-            password="PASSWORD",
-            email="mail2@test.com"
-        )
-        self.reservation1 = sample_reservation(user=self.user1)
-        self.reservation2 = sample_reservation(user=self.user2)
-        self.theatre_hall = sample_theatre_hall()
-        self.play = sample_play()
-        self.performance = sample_performance(play=self.play, theatre_hall=self.theatre_hall)
-        self.ticket1 = sample_ticket(row=1, seat=1, performance=self.performance, reservation=self.reservation1)
-        self.ticket2 = sample_ticket(row=2, seat=2, performance=self.performance, reservation=self.reservation2)
+        cls.client = APIClient()
+        cls.client.force_authenticate(user=cls.user1)
+
+        cls.reservation1 = sample_reservation(user=cls.user1)
+        cls.theatre_hall = sample_theatre_hall()
+        cls.play = sample_play()
+        cls.performance = sample_performance(play=cls.play, theatre_hall=cls.theatre_hall)
+        cls.ticket1 = sample_ticket(row=1, seat=1, performance=cls.performance, reservation=cls.reservation1)
+
     def test_ticket_list(self):
         serializer1 = TicketSerializer(self.ticket1)
         serializer2 = TicketSerializer(self.ticket2)
@@ -97,6 +105,7 @@ class AuthorizedTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn(serializer1.data, response.data)
         self.assertNotIn(serializer2.data, response.data)
+
     def test_ticket_detail(self):
         serializer = TicketSerializer(self.ticket1)
         response = self.client.get(get_ticket_retrieve_url(self.ticket1.id))
@@ -108,49 +117,40 @@ class AuthorizedTestCase(APITestCase):
             "row": 3,
             "seat": 3,
             "reservation": self.reservation1.id,
-            "performance": self.performance,
+            "performance": self.performance.id,
         }
-        response = self.client.post(TICKET_LIST_URL, ticket)
-        self.assertEqual(response.status_code,
-                         status.HTTP_201_CREATED)
+        response = self.client.post(TICKET_LIST_URL, ticket, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    # def test_ticket_update(self):
-    #     # First, create a ticket
-    #     create_ticket_data = {
-    #         "row": 1,
-    #         "seat": 13,
-    #         "performance": self.performance.id,
-    #         "reservation": self.reservation1.id,
-    #     }
-    #     response = self.client.post(TICKET_LIST_URL, create_ticket_data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #
-    #     # Retrieve the created ticket's ID
-    #     ticket_id = response.data['id']
-    #     update_ticket_url = get_ticket_retrieve_url(ticket_id)
-    #
-    #     # Define updated ticket data
-    #     updated_ticket_data = {
-    #         "row": 12,
-    #         "seat": 7,
-    #         # Optionally include performance and reservation if your API requires them
-    #         "performance": self.performance.id,
-    #         "reservation": self.reservation1.id,
-    #     }
-    #
-    #     # Perform the update
-    #     response = self.client.put(update_ticket_url, updated_ticket_data, format='json')
-    #
-    #     # Check if the update was successful
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)  # Assuming successful update returns 200 OK
-    #
-    # def test_ticket_delete(self):
-    #     ticket = sample_ticket(
-    #         row=1,
-    #         seat=3,
-    #         performance=self.performance,
-    #         reservation=self.reservation1
-    #     )
-    #     response = self.client.delete(get_ticket_retrieve_url(ticket.id))
-    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    #
+    def test_ticket_update(self):
+        create_ticket_data = {
+            "row": 1,
+            "seat": 13,
+            "performance": self.performance.id,
+            "reservation": self.reservation1.id,
+        }
+        response = self.client.post(TICKET_LIST_URL, create_ticket_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        ticket_id = response.data['id']
+        update_ticket_url = get_ticket_retrieve_url(ticket_id)
+
+        updated_ticket_data = {
+            "row": 12,
+            "seat": 7,
+            "performance": self.performance.id,
+            "reservation": self.reservation1.id,
+        }
+
+        response = self.client.put(update_ticket_url, updated_ticket_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_ticket_delete(self):
+        ticket = sample_ticket(
+            row=1,
+            seat=3,
+            performance=self.performance,
+            reservation=self.reservation1
+        )
+        response = self.client.delete(get_ticket_retrieve_url(ticket.id))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
